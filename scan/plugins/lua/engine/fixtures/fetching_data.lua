2423e5e2364d8032ac1430054c27bff1ac66d741
Nguyen Marc <nguyen_marc@live.fr>
2024-11-11 19:07:41 +0100

    chore(plugins): drop lua support

--- scan/plugins/lua/engine/fixtures/fetching_data.lua
+++
@@ -1,7 +0,0 @@
-local msgpack = require("msgpack")
-
-return msgpack.encode({
-  title = "Example Title",
-  release_date = 1682390400,   -- Unix timestamp
-  tags = { "tag1", "tag2", "tag3" }
-})
--- scan/plugins/lua/engine/fixtures/params.lua
+++
@@ -1,1 +0,0 @@
-return archive_path
